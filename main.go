3c937b025e19fe287be5b480377c29dd1a4710af
Ashraf Atef <ashraaf.ateef@gmail.com>
2024-09-22 21:18:07 +0200

    edit pr create logic

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"fmt"
 	"githubActivitiesCli/cmd"
 	"githubActivitiesCli/database"
+	"log"
+	"os"
 )
 
 func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(path) // for example /home/user
 	database.InitTables()
 	cmd.Execute()
 	defer database.CloseDB()
